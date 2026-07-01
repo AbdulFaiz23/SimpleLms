afd893d4dd71ae4ea72b771e19641a992f953715
AbdulFaiz23 <faizabdul856@gmail.com>
2026-07-01 23:23:40 +0700

    feat: redirect root url to api docs

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from lms.api import api
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/docs', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', api.urls),
 ]
